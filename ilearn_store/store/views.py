99dd57744638fbc5ed9efe445d14dc6bedfd8208
Patrycja <patrycjatyra@protonmail.com>
2020-09-10 18:10:35 +0100

    added update Item functions in veiws .py

--- ilearn_store/store/views.py
+++ ilearn_store/store/views.py
@@ -36,3 +36,29 @@ def store(request):
     products = Product.objects.all()
     context = {'products':products}
     return render(request, 'store/store.html', context)
+
+def updateItem(request):
+    	data = json.loads(request.body)
+	productId = data['productId']
+	action = data['action']
+	print('Action:', action)
+	print('Product:', productId)
+
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
+
+	orderItem.save()
+
+	if orderItem.quantity <= 0:
+		orderItem.delete()
+
+	return JsonResponse('Item was added', safe=False)
+

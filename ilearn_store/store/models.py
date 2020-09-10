c8ece97b1aadf59fcf4cecc241ad56f40f6ab472
Patrycja <patrycjatyra@protonmail.com>
2020-09-10 16:18:34 +0100

    adeed data

--- ilearn_store/store/models.py
+++ ilearn_store/store/models.py
@@ -3,27 +3,24 @@ from django.contrib.auth.models import User
 
 
 class Customer(models.Model):
-    user = models.OneToOneField(
-        User, null=True, blank=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200)
+   
 
-
-name = models.CharField(max_length=200, null=True)
-email = models.CharField(max_length=200)
-
-
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
-      name = models.CharField(max_length=200)
-      price = models.FloatField()
-      digital = models.BooleanField(default=False, null=True, blank=True)
-      image = models.ImageField(null=True, blank=True)
+    name = models.CharField(max_length=200)
+    price = models.FloatField(default=False)
+    digital = models.BooleanField(default=False, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
 
 @property
@@ -36,15 +33,14 @@ def imageURL(self):
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(
-        Customer, on_delete=models.SET_NULL, null=True, blank=True)
-date_ordered = models.DateTimeField(auto_now_add=True)
-complete = models.BooleanField(default=False)
-transaction_id = models.CharField(max_length=100, null=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
 
 
-def __str__(self):
-    return str(self.id)
+    def __str__(self):
+        return str(self.id)
 
 
 @property

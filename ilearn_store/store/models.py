563f0ed14c10ea65ac12db5d020bca2b5a99feef
Patrycja <patrycjatyra@protonmail.com>
2020-09-10 15:21:52 +0100

    added order model

--- ilearn_store/store/models.py
+++ ilearn_store/store/models.py
@@ -25,3 +25,24 @@ class Product(models.Model):
 		except:
 			url = ''
 		return url
+        
+class Order(models.Model):
+    	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+	date_ordered = models.DateTimeField(auto_now_add=True)
+	complete = models.BooleanField(default=False)
+	transaction_id = models.CharField(max_length=100, null=True)
+
+	def __str__(self):
+		return str(self.id)
+
+	@property
+	def get_cart_total(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.get_total for item in orderitems])
+		return total 
+
+	@property
+	def get_cart_items(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.quantity for item in orderitems])
+		return total 

c5625b7fa8e335dc9a2990d8e58876e4275852d7
Patrycja <patrycjatyra@protonmail.com>
2020-09-10 12:54:22 +0100

    added views functions

--- ilearn_store/store/views.py
+++ ilearn_store/store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def store(request):
+	context = {}
+	return render(request, 'store/store.html', context)
+
+def basket(request):
+	context = {}
+	return render(request, 'store/basket.html', context)
+
+def checkout(request):
+	context = {}
+	return render(request, 'store/checkout.html', context)

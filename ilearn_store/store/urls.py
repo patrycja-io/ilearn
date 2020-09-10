66a9077f8c9ffdefe86c6c28480a7801d8c7e771
Patrycja <patrycjatyra@protonmail.com>
2020-09-10 13:01:28 +0100

    typo removed

--- ilearn_store/store/urls.py
+++ ilearn_store/store/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 	#Leave as empty string for base url
 	path('', views.store, name="store"),
-	path('basket/', views.cart, name="basket"),
+	path('basket/', views.basket, name="basket"),
 	path('checkout/', views.checkout, name="checkout"),
 
 ]

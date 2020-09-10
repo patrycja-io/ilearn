0b4c3f059d5a2dd13bc8ae5a48c77361e7c94893
Patrycja <patrycjatyra@protonmail.com>
2020-09-10 15:17:02 +0100

    added customer model

--- ilearn_store/store/models.py
+++ ilearn_store/store/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+name = models.CharField(max_length=200, null=True)
+email = models.CharField(max_length=200)
+
+def __str__(self):
+		return self.name
